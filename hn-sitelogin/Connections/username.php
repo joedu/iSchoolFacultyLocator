1fa29d473e6cc88139b27590544df72b810fa861
Holden Nardini <hcn23@drexel.edu>
2012-03-10 11:29:16 -0500

    adding site login code

---
+++ hn-sitelogin/Connections/username.php
@@ -0,0 +1,10 @@
+<?php
+# FileName="Connection_php_mysql.htm"
+# Type="MYSQL"
+# HTTP="true"
+$hostname_username = "localhost";
+$database_username = "userlogin";
+$username_username = "administrator";
+$password_username = "Ch4ng3m3";
+$username = mysql_pconnect($hostname_username, $username_username, $password_username) or trigger_error(mysql_error(),E_USER_ERROR); 
+?>
---
+++ hn-sitelogin/login.php
@@ -0,0 +1,109 @@
+<?php require_once('Connections/username.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+?>
+<?php
+// *** Validate request to login to this site.
+if (!isset($_SESSION)) {
+  session_start();
+}
+
+$loginFormAction = $_SERVER['PHP_SELF'];
+if (isset($_GET['accesscheck'])) {
+  $_SESSION['PrevUrl'] = $_GET['accesscheck'];
+}
+
+if (isset($_POST['username'])) {
+  $loginUsername=$_POST['username'];
+  $password=$_POST['password'];
+  $MM_fldUserAuthorization = "";
+  $MM_redirectLoginSuccess = "http://68.81.93.94/ischoolfacultylocator/index.php";
+  $MM_redirectLoginFailed = "http://68.81.93.94/ischoolfacultylocator/hn-sitelogin/accessdenied";
+  $MM_redirecttoReferrer = false;
+  mysql_select_db($database_username, $username);
+  
+  $LoginRS__query=sprintf("SELECT username, password FROM users WHERE username=%s AND password=%s",
+    GetSQLValueString($loginUsername, "text"), GetSQLValueString($password, "text")); 
+   
+  $LoginRS = mysql_query($LoginRS__query, $username) or die(mysql_error());
+  $loginFoundUser = mysql_num_rows($LoginRS);
+  if ($loginFoundUser) {
+     $loginStrGroup = "";
+    
+	if (PHP_VERSION >= 5.1) {session_regenerate_id(true);} else {session_regenerate_id();}
+    //declare two session variables and assign them
+    $_SESSION['MM_Username'] = $loginUsername;
+    $_SESSION['MM_UserGroup'] = $loginStrGroup;	      
+
+    if (isset($_SESSION['PrevUrl']) && false) {
+      $MM_redirectLoginSuccess = $_SESSION['PrevUrl'];	
+    }
+    header("Location: " . $MM_redirectLoginSuccess );
+  }
+  else {
+    header("Location: ". $MM_redirectLoginFailed );
+  }
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>iSchool Faculty Locator Login</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+</head>
+
+<body>
+<img src="header_bg.png" width="979" height="239" />
+<div align="center">
+<form ACTION="<?php echo $loginFormAction; ?>" name="form1" method="POST">
+<table width="400" border="0" cellspacing="0" cellpadding="3">
+<tr>
+<td width="100">Username:</td>
+<td><input name="username" type="text" id="username"></td>
+</tr>
+<tr>
+<td width="100">Password:</td>
+<td><input name="password" type="password" id="password"></td>
+</tr>
+<tr>
+<td width="100"> </td>
+<td><input type="submit" name="Submit" value="Submit"></td>
+</tr>
+</table>
+</form>
+</div>
+</body>
+</html>
